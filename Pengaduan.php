f729c79c48ecd62d4989c6e7e93fb9d76cad3bae
RewisCollection <144434634+RewisCollection@users.noreply.github.com>
2023-09-08 16:06:16 +0700

    Add files via upload

---
+++ Pengaduan.php
@@ -0,0 +1,140 @@
+<?php
+include 'koneksi.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link href="upfoto/pengaduan masyarakat.png" rel="shortcut icon">
+<a href="index.php"> <img src="upfoto/tombol kembali.png"width="100px" height="100px"></a>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Rewis Collection's</title>
+</head>
+
+<table width="1300 " border="2" cellspacing="5" cellpadding="60px">
+<tr>
+<body><center>
+ <br> <br> <br> <br> <br> <br>
+   <td colspan="3" align="center"><p> <b>Etalase ( 1 )</b></p> <center> <br> 1. ROK MARLENA / MADURA DEWASA <center> 2. ROK MARLENA / MADURA ANAK-ANAK 
+   <center><a href="lihat1.php"> <br> <input type="submit" name="lihat" value="LIHAT" style="font-size:15px"> </td>
+  
+   <td colspan="3" align="center"> <p> <b> Etalase ( 2 ) </b></p> <center> <br> 1. ALL SELEMBONG <center> 2 . SARUNG BALI DEWASA + ANAK ( KOMPLIT SAMA 
+   UDENG ) <center> 3 . KEMEJA ATASAN BALI ANAK <center>4 . CELANA BETAWI <center> 5 . BAJU PONOROGO ANAK COWOK <center> 6 . CONTOH KERUDUNG HITAM 
+   <center> 7 . SETELAN BASOFI ANAK COWOK <center> 8 . ATASAN BESKAP WARNA HITAM ANAK <center>9 . SETELAN BESKAP BAYI <center> 10 . ATASAN LURIK BAYI 
+   <center> 11 . KEBAYA MODERN ANAK <center> <br> <a href="lihat2.php"><input type="submit" name="login" value="LIHAT" style="font-size:15px"/>  </td>
+   
+   <td colspan="3" align="center"> <p> <b> Etalase ( 3 ) </b></p> <center> <br> 1 . KEBAYA MODERN ( DEWASA ) <center> 2 . KAMEN DEWASA <center> 3 . 
+   KAMEN ANAK <center>4 . KAMEN BALI <center> 5 . ROK LILIT <center> 6 . SELENDANG <center> 7 . BANDO LURIK <center> <br> <a href="lihat3.php"><input 
+   type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+</tr>
+<tr>
+   <td colspan="3" align="center"><p> <b>Etalase ( 4 )</b></p> 1 . BAJU KAOS MERAH PUTIH SAKERA / MADURA ( DEWASA )<center> 2 . BAJU KAOS MERAH PUTIH SAKERA / MADURA ( ANAK-ANAK ) <center> 3 . BAJU SAKERA SET / MADURA ANAK ( SD / UK. M )( CELANA + ROMPI ) <center> 4 . BAJU SAKERA SET / MADURA ANAK( ( SMP / UK. L1 )( CELANA + ROMPI )<center><a href="lihat4.php"> <br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+    
+   <td colspan="3" align="center"><p> <b>Etalase ( 5 )</b></p> 1 . KEBAYA LURIK COWOK & CEWEK DEWASA <center> 2 . KEBAYA JUMPUTAN BIRU <center>3 . KEBAYA PUTIH BIG SIZE JADUL  ( DEWASA ) <center> 4 . KEBAYA JADUL STANDART ( BIRU,HIJAU,KUNING ) <center> 5 . KAMEN + SAPUT BALI COWOK DEWASA ( PUTIH & KOTAK-KOTAK ) <center> 6 . UDENG <center> 7 . STAGEN<center><a href="lihat5.php"> <br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+   
+   <td colspan="3" align="center"><p> <b>Etalase ( 6 ) </b></p>1 . MANSET ANAK <center> 2 . MANSET DEWASA <center> 3 . LATORSO, SABUK BALI, KEMBEN <center> 4 . BAJU DODOTAN MANUAL (DEWASA & ANAK-ANAK) <center> 5 . BAJU DODOTAN PRAKTIS ( ANAK )<center><a href="lihat6.php"><br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+</tr>
+<tr>
+   <td colspan="3" align="center"><p><b>Etalase ( 7 )</b></p> 1 . BAJU SAKERA SET/MADURA DEWASA ( L3 )( CELANA + ROMPI )<center> 2 . BAJU SAKERA SET/MADURA DEWASA ( L4 )( CELANA + ROMPI ) <center> 3 . BAJU SAKERA SET/MADURA DEWASA ( L5 )( CELANA + ROMPI ) <center><a href="lihat7.php"><br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+    
+   <td colspan="3" align="center"><p><b>Etalase ( 8 )</b></p> 1 . BAJU GAUN <center> 2 . BAJU POLISI ANAK DAN DEWASA <center>3 . BAJU TENTARA ANAK DAN DEWASA <center><a href="lihat8.php"> <br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+   
+   <td colspan="3" align="center"><p><b>Etalase ( 9 ) BAJU PROFESI </b></p>1 . VETERAN ANAK COWOK & CEWEK + TOPI <center> 2 . ANGKATAN LAUT ANAK COWOK & CEWEK <center> 3 . DOKTER ANAK + TOPI <center> 4 . SUSTER ANAK + TOPI <center> 5 . PRAMUGARI ANAK ( TK ) BIRU & MERAH + TOPI <center> 6 . PILOT ANAK ( TK ) <center> 7 . ASTRONOT ANAK + TOPI <center> 8 . CHEEF ANAK + TOPI <center>9 . PILOT ANGKATAN UDARA ANAK + TOPI <center> 10 . PELAUT ANAK + TOPI <center><a href="lihat9.php"> <br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+</tr>
+<tr>
+   <td colspan="3" align="center"><p><b>Etalase ( 10 )</b></p> 1 . SUNTIANG <center> 2 . BANDO BUNGAN BALI <center> 3 . BANDO TARI BALI <center> 4 . PIRING BALI <center> 5 . BARET MERAT <center> 6 . TOPI FASHION <center> 7 . STETOSKOP ( ALAT DOKTER / SUSTER )<center><a href="lihat10.php"><br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td> 
+   
+   <td colspan="3" align="center"><p><b>Etalase ( 11 )</b></p> 1 . BAJU DANGDUT <center> 2 . KAIN <center> 3 . SARUNG TANGAN<center> 4 . DEKKER TANGAN <center>5 . STOCKING <center><a href="lihat11.php"> <br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+   
+   <td colspan="3" align="center"><p><b>Etalase ( 12 )</b></p>1 . BAJU SLEBEWWW <center>2 . BANDO BULU MERAH PUTIH SLEBEWWW <center>3 . KAOS KAKI SLEBEWWW <center> 4 . ROK SLEBEWWW <center><a href="lihat12.php"><br> <input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+  
+</tr>
+<tr>
+   <td colspan="3" align="center"><p><b>Etalase ( 13 )</b></p>1 . BAJU DAYAK HITAM BLUDRU ( ANAK-ANAK & DEWASA ) <center> 2 . BAJU DAYAK MERAH BLUDRU ( ANAK-ANAK & DEWASA )<center> 3 . CINDE <center><a href="lihat13.php"><br> <input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td> 
+   
+   <td colspan="3" align="center"><p><b>Etalase ( 14 )</b></p>1 . KEBAYA MARLENA / MADURA ( ANAK - ANAK ) <center> 2 . KEBAYA MARLENA / MADURA ( DEWASA) <center><a href="lihat14.php"><br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+   
+   <td colspan="3" align="center"><p><b>Etalase ( 15 )</b></p>1 . KEBAYA BALI KUNING ( DEWASA, UK. L & XXL ) <center>2 . KEBAYA BALI PUTIH LENGAN PENDEK + SABUK ( DEWASA )<center> 3 . KEBAYA BALI PUTIH LENGAN PANJANG ( DEWASA )<center> 4 . KEBAYA BALI COKLAT LENGAN PANJANG ( ANAK-ANAK )<center>5 . KEBAYA BALI ABU LENGAN PANJANG ( DEWASA )<center> 6 . KEBAYA BALI HITAM LENGAN 7/8 ( DEWASA )<center>7 . KEBAYA BALI HITAM LENGAN PANJANG ( DEWASA )<center> 8 . KEBAYA BALI ANAK SD HIJAU <center> 9 . SETELAN KEBAYA BALI ANAK SD PUTIH BIRU <center> 10 . SETELAN KEBAYA BALI BAYI + SABUK<center> 11 . BAJU BALI LANGSUNGAN DEWASA ( TRADISIONAL  PRAKTIS )<center> 12 . BAJU BALI LEMBARAN DEWASA & ANAK ( TRADISIONAL MANUAL ) <center> 13 . KEBAYA BALI PUTIH LENGAN PANJANG ( ANAK )<center> 14 . SETELAN BAJU BALI ANAK KEMBEN TRADISIONAL PRAKTIS <center><a href="lihat15.php"><br><input type="submit" name="login" value="LIHAT" style="font-size:15px " /> </td>
+</tr>
+</table>
+
+
+<body background="upfoto/rewis123.jpg">
+<br /><br /><br /><br /><br /><br /><br /><br />
+
+<form action="" method="post">
+<table border="1" align="center" bgcolor="#FFFFFF">
+	<tr>
+	 <style type="text/css">
+        body { font-family: Calibri, Helvetica, Arial, sans-serif; }
+        h3 { font-family: Cambria,"Times New Roman",serif; }
+        #paragraf2 { font-family: Georgia, serif; }
+    </style>
+		<td colspan="3" align="center">  <p>
+        <strong>PESAN SESUAI ETALASE</strong> 
+    </p>
+	</tr>
+	<tr>
+		<td>ETALASE ke dan Nomor BAJU ADAT</td>
+		<td>:</td>
+		<td><input type="text" name="tgl"/></td>
+	</tr>
+	<tr>
+		<td>KEPADA/NAMA</td>
+		<td>:</td>
+		<td><input type="text" name="nik" /></td>
+	</tr>
+	<tr>
+		<td>ALAMAT</td>
+		<td>:</td>
+		<td><textarea name="keluhan" rows="5" cols="21"/></textarea></td>
+	</tr>
+	<tr>
+		<td>NO HP/NO WHATSAPP</td>
+		<td>:</td>
+		<td><input type="text" name="foto" /></td>
+	</tr>
+	
+	<tr>
+		<td colspan="3" align="center"> <input type="submit" name="simpan" value="PESAN" /> 
+	</tr>
+</table>
+</form>
+<?php 
+$tgl=@$_POST['tgl'];
+$nik=@$_POST['nik'];
+$keluhan=@$_POST['keluhan'];
+$foto=@$_POST['foto'];
+
+
+if(isset($_POST['simpan'])){
+$filename=$_FILES['foto']['tmp_name'];
+$dest="upfoto/".$_FILES['foto']['name'];
+if(move_uploaded_file($filename,$dest)){
+list($tinggi,$lebar)=getimagesize($dest);
+$file_baru=imagecreatefromjpeg($dest);
+$warna_baru=imagecreatetruecolor($lebar, $tinggi);
+imagecopyresampled($warna_baru,$file_baru,0,0,0,0,$lebar,$tinggi,$lebar,$tinggi);
+imagejpeg($warna_baru,$dest,25);
+echo'berhasil';
+}
+if($nik==""||$keluhan==""){
+?>
+<script type="text/javascript">
+alert("Silahkan Pesan Kembali...!!!");
+</script>
+<?php
+}else{
+mysql_query("INSERT INTO tbpengaduan(tglpengaduan,nik,isi,foto)VALUES('".$tgl."','".$nik."','".$keluhan."','".$foto."')");
+echo "<script> alert('Trimakasih, Pesanan Anda Sudah Sukses'); </script>";
+header('location:http://localhost/rewis/index.php?page=pengaduan');
+}}
+?>
+<?php
+$sql=mysql_query("SELECT * FROM tbpengaduan");
+while ($data=mysql_fetch_array($sql)){
+?>
+<?php }?>
+</table>
+
+</body>
+</html>
